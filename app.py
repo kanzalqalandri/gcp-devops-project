e3d2184d4600f04779266e7555ac9307488e2fe3
Kanzal Qalandri <kanzalqalandri@gmail.com>
2023-09-09 09:48:04 +0500

    replicas updated in dev.

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Kanzal Qalandri is Learning GCP DevOps. And this is DEV Branch'
+    return 'Hello, Kanzal Qalandri is Learning GCP DevOps.'
